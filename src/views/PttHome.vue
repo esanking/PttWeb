ad0ee86ec9bcc369b63d731e21e896456a19ff2a
esanking <asd500373@gmail.com>
2022-06-13 23:42:41 +0800

    調整UI顏色、調整打API_delay_time、調整loading機制

--- src/views/PttHome.vue
+++ src/views/PttHome.vue
@@ -1,23 +1,33 @@
 <template>
-  <div class="w-100 MtMargin bg-info">
-    <div class="header pt-4">
-      <h3 class="w-100 text-center text-success">Stock PTT</h3>
-      <div v-for="item in text" :key="item" class="d-flex justify-content-center">
-        <div class="bg-white w-75 d-flex justify-content-between mt-2" @click="article(item)">
-          <div>
-            <p>作者: {{item.author}}</p>
-            <p>標題: {{item.title}}</p>
-            <p>時間: {{item.releaseTime}}</p>
-          </div>
-          <div v-if="item.imgList.length > 0">
-            <img :src="item.imgList[0]" :alt="item.title">
+  <div class=" bg-indigo w-100" style="height:100vh;">
+    <div class="w-100 bg-indigo">
+      <div class="header pt-4">
+        <h3 class="w-100 text-center text-purple">Stock PTT</h3>
+        <div v-for="item in text" :key="item" class="d-flex justify-content-center">
+          <div class="bg-white w-75 d-flex justify-content-between mt-2" @click="article(item)">
+            <div>
+              <p>作者: {{item.author}}</p>
+              <p>標題: {{item.title}}</p>
+              <p>時間: {{item.releaseTime}}</p>
+            </div>
+            <div v-if="item.imgList.length > 0">
+              <img :src="item.imgList[0]" :alt="item.title">
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="d-flex justify-content-center">
-      <div class="spinner-border" role="status">
-        <span class="visually-hidden">Loading...</span>
+      <div class="d-flex justify-content-center" v-if="loading">
+        <div class="spinner-border text-purple" role="status">
+          <span class="visually-hidden"></span>
+        </div>
+      </div>
+      <div class="d-flex justify-content-center" v-else>
+        <h5 class=" text-danger">
+          載入失敗
+          <span class="spinner-grow me-1" style="width: .5rem; height: .5rem;" role="status" />
+          <span class="spinner-grow me-1" style="width: .5rem; height: .5rem;" role="status" />
+          <span class="spinner-grow" style="width: .5rem; height: .5rem;" role="status" />
+        </h5>
       </div>
     </div>
   </div>
@@ -42,13 +52,14 @@
       let data = ref({});
       let staticBackdrop = ref(null);
       let page = ref(1);
+      let loading = ref(true);
 
       onMounted(() => {
         GetRecord();
         staticBackdrop.value = new Modal(document.getElementById('staticBackdrop'), {
           keyboard: false
         })
-        window.onscroll = throttle(GetRecord, 1000)
+        window.onscroll = throttle(GetRecord, 3000)
 
       })
       function throttle(fn, delay) {
@@ -57,6 +68,7 @@
           if (flag) {
             setTimeout(() => {
               if (window.scrollY + window.innerHeight >= document.body.offsetHeight - 100) {
+                loading.value = true;
                 page.value++;
                 fn.apply(this, arguments);
               }
@@ -69,9 +81,13 @@
       async function GetRecord() {
         const api = 'https://protected-chamber-55237.herokuapp.com/ptt/getList';
         // const api = 'http://localhost:3000/ptt/getList';
-        proxy.$http.get(api, { params: { page: page.value } }).then((response) => {
-          text.value = text.value.concat(response.data.data)
-        });
+        proxy.$http.get(api, { params: { page: page.value } })
+          .then((response) => {
+            text.value = text.value.concat(response.data.data)
+          })
+          .catch((req) => {
+            loading.value = false;
+          });
       }
       function article(item) {
         data.value = item;
@@ -82,7 +98,8 @@
         text,
         article,
         // active,
-        data
+        data,
+        loading
       }
     }
   });
